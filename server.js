f4519ad395a2fd63987b7fbf7cb15d9c2b6d6335
fritzgt <tuanis@live.com>
2019-08-07 10:44:44 -0400

    completed create and read from CRUD

--- server.js
+++ server.js
@@ -6,4 +6,36 @@ const server = express();
 
 server.use(express.json());
 
+//Create
+server.post('/', async (req, res) => {
+  const newData = req.body;
+  try {
+    const data = await db('accounts').insert(newData);
+    res.status(200).json({ data });
+  } catch {
+    res.status(500).json({ message: 'Could not connect' });
+  }
+});
+
+//Read
+server.get('/', async (req, res) => {
+  try {
+    // const data = await db('accounts');
+    const data = await db.select('*').from('accounts');
+    res.status(200).json({ data });
+  } catch {
+    res.status(500).json({ message: 'Could not connect' });
+  }
+});
+
+//Update
+server.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+  } catch {
+    res.status(500).json({ message: 'Could not connect' });
+  }
+});
+
 module.exports = server;
